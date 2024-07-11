0bb68cc96d69a9f33926a40a55554dfea4048d17
Dasun_Bhagya <130273042+DBhagya23@users.noreply.github.com>
2024-07-11 17:49:55 +0530

    Add files via upload
    added utilz and input output abilities of the sample main character.. Can identify collision in interface

--- RKnight35_The_2D_Game/src/Game_Main/GamePanel.java
+++ RKnight35_The_2D_Game/src/Game_Main/GamePanel.java
@@ -1,20 +1,43 @@
 package Game_Main;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+import static Game_Main.Game.GAME_HEIGHT;
+import static Game_Main.Game.GAME_WIDTH;
 
 public class GamePanel extends JPanel {
 
-		public GamePanel() {
-			
-		}
-		public void paintComponent(Graphics g) {
-			
-			super.paintComponent(g);
-			
-			g.fillRect(100,100,200,50);
-			
-		}
+	private MouseInputs mouseInputs;
+	private Game game;
+
+	public GamePanel(Game game) {
+		mouseInputs = new MouseInputs(this);
+		this.game = game;
+		setPanelSize();
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+		setPreferredSize(size);
+	}
+
+	public void updateGame() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		game.render(g);
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
 }
--- RKnight35_The_2D_Game/src/Game_Main/MainClass.java
+++ RKnight35_The_2D_Game/src/Game_Main/MainClass.java
@@ -3,9 +3,10 @@ package Game_Main;
 public class MainClass {
 
 	public static void main(String[] args) {
-
-		Game game = new Game();		
+		new Game();
+		
+		
+		
 	}
-
 
 }
